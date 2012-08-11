cc4711e9afeb7d4d52b6e1340950be138154399d
Katsunori Kawaguchi <hamakata@gmail.com>
2012-08-11 19:07:04 +0900

    Use symlink instead of gitignore
    gitignoreを使うのを止めて、dotfiles内へのsymlinkを貼るようにした

---
+++ init.sh
@@ -0,0 +1,6 @@
+ln -s dotfiles/.zshrc .zshrc
+ln -s dotfiles/.zshenv .zshenv
+ln -s dotfiles/.screenrc .screenrc
+ln -s dotfiles/.vimrc .vimrc
+ln -s dotfiles/.gitconfig .gitconfig
+ln -s dotfiles/.gemrc .gemrc
